c8d84a70752ea6c049d611345382374f9e2b0972
65600 <37107229+65600@users.noreply.github.com>
2020-01-14 15:34:19 +0100

    added P2SH signing

--- src/main/scala/Signer.scala
+++ src/main/scala/Signer.scala
@@ -7,9 +7,16 @@ class Signer {
 
   // Generate signature from "dummy" P2PKH redeemScript
   def signP2PKH(priv : PrivateKey, toSign : Transaction, inputIndex : Int) : ByteVector = {
-    Transaction.signInput(toSign, inputIndex, genP2PKHDummy(priv.publicKey),bitcoin.SIGHASH_ALL, Satoshi(0),SigVersion.SIGVERSION_BASE,priv)
+    Transaction.signInput(toSign, inputIndex, genP2PKHDummy(priv.publicKey), bitcoin.SIGHASH_ALL, Satoshi(0), SigVersion.SIGVERSION_BASE, priv)
   }
 
+  // Generate signature from a P2SH input script.
+  def signP2SH(priv : PrivateKey, toSign : Transaction, inputIndex : Int) : ByteVector = {
+    // Assume P2SH redeem script is the very last chunk in our half finished input tx
+    val redeemScript = Script.write(Script.parse(toSign.txIn(inputIndex).signatureScript).last :: Nil)
+    Transaction.signInput(toSign, inputIndex, redeemScript, bitcoin.SIGHASH_ALL, Satoshi(0), SigVersion.SIGVERSION_BASE, priv)
+  }
+  
   // Generate standard P2PKH redeem script from expected public key
   def genP2PKHDummy(pub : PublicKey) : ByteVector = {
     val pkh = Crypto.hash160(pub.value)
