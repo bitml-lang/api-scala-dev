1056ca9e694c4e57f612360ea6ceeafb95b3c724
65600 <37107229+65600@users.noreply.github.com>
2020-04-02 16:09:00 +0200

    Do not include secrets in serialization.

---
+++ src/main/scala/xyz/bitml/api/serialization/ChunkEntrySerializer.scala
@@ -0,0 +1,36 @@
+package xyz.bitml.api.serialization
+
+import fr.acinq.bitcoin.Crypto.PublicKey
+import org.json4s.{CustomSerializer, Extraction, Formats}
+import org.json4s.JsonAST.{JField, JNothing, JNull, JObject}
+import scodec.bits.ByteVector
+import xyz.bitml.api.ChunkType.ChunkType
+import xyz.bitml.api.{ChunkEntry, ChunkType}
+
+
+class ChunkEntrySerializer extends CustomSerializer[ChunkEntry](format =>(
+  {
+    case JObject(l : List[JField])=>
+      implicit val formats: Formats = org.json4s.DefaultFormats + new org.json4s.ext.EnumSerializer(ChunkType) + new ByteVectorSerializer()
+      val lm = l.toMap
+      val desType = Extraction.extract[ChunkType](lm("type"))
+      val desPos = Extraction.extract[Int](lm("index"))
+      val desOwn = Extraction.extractOpt[PublicKey](lm.getOrElse("owner", JNull))
+      val desVal = Extraction.extract[ByteVector](lm("data"))
+      ChunkEntry(desType, desPos, desOwn, desVal)
+  },
+  {
+    case x: ChunkEntry => {
+      val secretive = true
+      implicit val formats: Formats = org.json4s.DefaultFormats + new org.json4s.ext.EnumSerializer(ChunkType) + new ByteVectorSerializer()
+
+      val serType = JField("type", Extraction.decompose(x.chunkType))
+      val serPos = JField("index", Extraction.decompose(x.chunkIndex))
+      val serOwn = JField("owner", Extraction.decompose(x.owner))
+      val serVal = JField("data", if (secretive && (x.chunkType == ChunkType.SECRET_IN || x.chunkType == ChunkType.SECRET_WIT))
+        Extraction.decompose(ByteVector.empty) else Extraction.decompose(x.data))
+      val obj = List(serType, serPos, serOwn, serVal)
+      JObject(obj)
+    }
+  }
+))
--- src/main/scala/xyz/bitml/api/serialization/MetaStorageSerializer.scala
+++ src/main/scala/xyz/bitml/api/serialization/MetaStorageSerializer.scala
@@ -10,13 +10,13 @@ class MetaStorageSerializer extends CustomSerializer[MetaStorage](format =>(
 
   {
     case JObject(l : List[(String, JValue)]) =>
-      implicit val formats: Formats = org.json4s.DefaultFormats + new org.json4s.ext.EnumSerializer(ChunkType) + new ByteVectorSerializer() + new SatoshiSerializer()
+      implicit val formats: Formats = org.json4s.DefaultFormats + new ChunkEntrySerializer + new SatoshiSerializer
       val l1 = l.map(a => (a._1, Extraction.extract[TxEntry](a._2)))
       new MetaStorage(l1.toMap)
   },
   {
     case x: MetaStorage => {
-      implicit val formats: Formats = org.json4s.DefaultFormats + new org.json4s.ext.EnumSerializer(ChunkType) + new ByteVectorSerializer() + new SatoshiSerializer()
+      implicit val formats: Formats = org.json4s.DefaultFormats + new ChunkEntrySerializer + new SatoshiSerializer
       val x1 = x.dump().toList
       val f = (a : (String, TxEntry)) => JField(a._1, Extraction.decompose(a._2))
       val l1 = x1.map(f)
